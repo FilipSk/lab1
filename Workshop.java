8368f7d27c7b65de1972c3ea6619a4167520b850
AlexanderSashaa <alexag@chalmers.se>
2025-02-11 13:09:28 +0100

    Fråga 2 klar

--- Workshop.java
+++ Workshop.java
@@ -1,4 +1,15 @@
-public class Workshop {
+import java.util.ArrayList;
 
+public class Workshop {
+    private int maxCars;
+    private ArrayList<Car> cars = new ArrayList<>();
+
+    public Workshop(int maxCars) {
+        this.maxCars = maxCars;
+    }
+
+    private void loadCar() {
+
+    }
 
 }
